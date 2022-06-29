d9ca34a964748e767a0c3708db2f253277d2c4da
inord777dev <inord777dev@gmail.com>
2022-06-29 11:55:14 +0300

    feat: add .env.example

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-const dirname = process.env.dirname || path.resolve(path.dirname('.repo/nodejs/v16.15.1'));
+const dirname = process.env.PATH_REPO || path.resolve(path.dirname('repo/nodejs/v16.15.1'));
 
 function fileDownload(res, fileName) {
   const fullName = path.join(dirname, fileName);
   var file = fs.readFileSync(fullName, 'binary');
-  console.log(fullName);
 
   res.setHeader('Content-Length', file.length);
   res.write(file, 'binary');
   res.end();
+
+  console.log(`Download ${fullName}`);
 }
 
 app.get('/v16.15.1/node-v16.15.1-headers.tar.gz', function (req, res) {
@@ -36,6 +37,8 @@ app.get('/v16.15.1/win-x64/node.lib', function (req, res) {
 //   fileDownload(res, '/v16.15.1/win-arm64/node.lib');
 // });
 
-app.listen(8080, () => {
-  console.log('listen 8080');
+const port = Number.parseInt(process.env.PORT) || 8080
+
+app.listen(port, () => {
+  console.log(`listen ${port}`);
 });
